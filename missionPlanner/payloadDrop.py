de90f7a9fbbc42f1bcb8040447f24aa9741b4e35
betaBison <knowles.derek@gmail.com>
2019-06-01 12:20:24 -0600

    threshold boundary

--- missionPlanner/payloadDrop.py
+++ missionPlanner/payloadDrop.py
@@ -2,11 +2,13 @@
 
 import rospy
 from rosplane_msgs.msg import Waypoint
+import numpy as np
 
 class PayloadDrop():
     def __init__(self):
         self.dropWaypoint = rospy.get_param('DROP_LOCATION')    # the drop waypoint
         self.nextWaypoint = False                               # true if the next waypoint is the drop waypoint
+        self.threshold = 2.0
 
         rospy.Subscriber('/current_waypoint',Waypoint,self.check)
 
@@ -19,14 +21,16 @@ class PayloadDrop():
         print("current waypoint =",currentWaypoint)
 
         if self.nextWaypoint:
-            print("\n\n\n\n\n  PAYLOAD DROP IS UP NEXT!!! \n\n\n\n\n")
-            if not (all(currentWaypoint == dropWaypoint)):
-                # drop the payload!
-                print("\n\n\n\n\n  WE HAVE ATTEMPTED PAYLOAD DROP!!!   \n\n\n\n\n")
-                rospy.ServiceProxy('arm payload',arm_bomb) and rospy.ServiceProxy('drop payload',actuate_drop_bomb)
-
+            if not (np.linalg.norm(np.array(currentWaypoint) - np.array(self.dropWaypoint)) < self.threshold):
+                ii = 0
+                while ii < 10:
+                    # drop the payload!
+                    print("\n\n\n\n\n  WE HAVE ATTEMPTED PAYLOAD DROP!!!   \n\n\n\n\n")
+                    rospy.ServiceProxy('arm payload',arm_bomb) and rospy.ServiceProxy('drop payload',actuate_drop_bomb)
+                    ii += 1
         else:
-            if currentWaypoint == self.dropWaypoint:
+            print("\n\n\n\n\n  PAYLOAD DROP IS UP NEXT!!! \n\n\n\n\n")
+            if np.linalg.norm(np.array(currentWaypoint) - np.array(self.dropWaypoint)) < self.threshold:
                 self.nextWaypoint = True
 
 
