5db9cef85ec3fa645337b22f07c7c4084db9bf64
Sequoia Ploeg <sequoiap4@gmail.com>
2020-02-28 09:21:30 -0700

    Logging color formatter

--- src/metis/__init__.py
+++ src/metis/__init__.py
@@ -8,14 +8,44 @@ Metis
 
 import logging
 
+from copy import copy
+from logging import Formatter
+
+MAPPING = {
+    'DEBUG'   : 37, # white
+    'INFO'    : 36, # cyan
+    'WARNING' : 33, # yellow
+    'ERROR'   : 31, # red
+    'CRITICAL': 41, # white on red bg
+}
+
+PREFIX = '\033['
+SUFFIX = '\033[0m'
+
+class ColoredFormatter(Formatter):
+
+    def __init__(self, patern):
+        Formatter.__init__(self, patern)
+
+    def format(self, record):
+        colored_record = copy(record)
+        levelname = colored_record.levelname
+        seq = MAPPING.get(levelname, 37) # default white
+        colored_levelname = ('{0}{1}m{2}{3}') \
+            .format(PREFIX, seq, levelname, SUFFIX)
+        colored_record.levelname = colored_levelname
+        return Formatter.format(self, colored_record)
+
 from metis.core import *
 
 # Set up logger
-logger = logging.getLogger('METIS')
+# logger = logging.getLogger('METIS')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(levelname)s] [%(asctime)s] %(name)s: %(message)s')
+# formatter = logging.Formatter('[%(levelname)s] [%(asctime)s] %(name)s: %(message)s')
+formatter = ColoredFormatter('[%(levelname)s] [%(asctime)s] %(name)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
