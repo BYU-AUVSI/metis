8768f6815c83747d6ef6123c5b496cca967c9ffa
Sequoia Ploeg <sequoiap4@gmail.com>
2020-04-11 00:02:46 -0600

    Save more dubins params

--- src/metis/rrt/rrt_dubins.py
+++ src/metis/rrt/rrt_dubins.py
@@ -15,9 +15,8 @@ from .animation import Animation2D
 _module_logger = logging.getLogger(__name__)
 
 dubins_config = Config(
-    # clearance=20,
     max_distance = 150.0,
-    min_radius = 50.0,
+    min_radius = 30.0,
     iterations=3,
 )
 
@@ -32,7 +31,6 @@ class DubinsRRT(RRT):
 
     def __init__(self, mission, animate=True, config=dubins_config):
         super(DubinsRRT, self).__init__(mission, animate=animate, config=config)
-        self.dubin_params = DubinsParameters()
 
     def find_full_path(self, waypoints, connect=False):
         """
@@ -66,8 +64,10 @@ class DubinsRRT(RRT):
         full_path = self.filter_duplicate_waypoints(full_path)
         if self.animation:
             for way1, way2 in zip(full_path[:-1], full_path[1:]):
-                ned = self.points_along_path(way1, way2)
+                ned = self.points_along_path(way1, way2, self.config.min_radius, self.config.resolution)
                 self.animation.add_path(ned, True)
+            for way in full_path:
+                self.animation.add_waypoint(way.n, way.e, way.d)
         return full_path
 
     def find_path(self, start, end):
@@ -100,7 +100,7 @@ class DubinsRRT(RRT):
         # check for if solution at the beginning    
         chi = heading(start_node, end_node)
         if self.flyable_path(start_node, end_node, start_node.chi, chi):
-            self.animation.add_path(self.points_along_path(start_node, end_node)) if self.animation else None
+            self.animation.add_path(self.points_along_path(start_node, end_node, self.config.min_radius, self.config.resolution)) if self.animation else None
             return [convert_point(start_node, Waypoint), convert_point(end_node, Waypoint)]  # Returns the two waypoints as the succesful path
 
         #START NEW TESTING CODE
@@ -215,7 +215,7 @@ class DubinsRRT(RRT):
         _logger = _module_logger.getChild('flyable_path')
 
         #check for obstacles and boundaries
-        ned = self.points_along_path(start, end)
+        ned = self.points_along_path(start, end, self.config.min_radius, self.config.resolution)
 
         if collision(ned, self.mission.boundary_poly, self.mission.obstacles, self.config.clearance):
             _logger.critical('COLLISION')
@@ -240,7 +240,8 @@ class DubinsRRT(RRT):
 
         return True
 
-    def points_along_path(self, start, end):
+    @staticmethod
+    def points_along_path(start, end, radius, res):
         """
         Creates a stepped range of values from the starting node to the ending node
         with difference in step size guaranteed to be less than `self.config.resolution`.
@@ -251,6 +252,10 @@ class DubinsRRT(RRT):
             The starting node to create a range from.
         end : metis.rrt.rrt_base.Node
             The ending node to create a range to.
+        radius : float
+            The radius of the arcs generated for each end of the Dubins path.
+        res : float
+            The maximum distance step between points along the path.
         
         Returns
         -------
@@ -259,15 +264,15 @@ class DubinsRRT(RRT):
             down points.
         """
         # Update the dubins parameters first
-        params = self.dubin_params.update(start, end, self.config.min_radius)
+        params = DubinsParameters.update(start, end, radius)
         
         # Use values from the dubins parameters for the points_along_path functions
         if not params.valid:
             return None
 
-        ned_s = points_along_arc(params.ps, params.r1, params.cs, self.config.min_radius, params.ds, self.config.resolution)
-        ned_m = points_along_straight(params.r1, params.r2, self.config.resolution)
-        ned_e = points_along_arc(params.r2, params.pe, params.ce, self.config.min_radius, params.de, self.config.resolution)
+        ned_s = points_along_arc(params.ps, params.r1, params.cs, radius, params.ds, res)
+        ned_m = points_along_straight(params.r1, params.r2, res)
+        ned_e = points_along_arc(params.r2, params.pe, params.ce, radius, params.de, res)
         ned = np.concatenate((ned_s, ned_m, ned_e), axis=0)
 
         # import matplotlib.pyplot as plt
@@ -360,7 +365,7 @@ class DubinsRRT(RRT):
 
         # Add this new node to the tree of viable paths.
         tree.add(new_node)
-        self.animation.add_path(self.points_along_path(closest, new_node)) if self.animation else None
+        self.animation.add_path(self.points_along_path(closest, new_node, self.config.min_radius, self.config.resolution)) if self.animation else None
 
         # Check to see if the new node can connect to the end node.
         chi = heading(new_node, end)
@@ -370,7 +375,7 @@ class DubinsRRT(RRT):
         if self.flyable_path(new_node, end, new_node.chi, end.chi):
             end_n = Node(end.n, end.e, end.d, chi, new_node.cost + end.distance(new_node), new_node, True)
             tree.add(end_n)
-            self.animation.add_path(self.points_along_path(new_node, end_n)) if self.animation else None
+            self.animation.add_path(self.points_along_path(new_node, end_n, self.config.min_radius, self.config.resolution)) if self.animation else None
             return tree, True
         else:
             return tree, False
@@ -385,7 +390,7 @@ class DubinsRRT(RRT):
             prev_node = smoothed_path[-1]
             considered = path[j]
             next_node = path[j + 1]
-            points = self.points_along_path(prev_node, next_node)
+            points = self.points_along_path(prev_node, next_node, self.config.min_radius, self.config.resolution)
             if collision(points, self.mission.boundary_poly, self.mission.obstacles, self.config.clearance):
                 smoothed_path.append(considered)
         smoothed_path.append(path[-1])
@@ -394,25 +399,8 @@ class DubinsRRT(RRT):
 
 
 class DubinsParameters:
-    def __init__(self):
-        self.p_s = np.inf*np.ones((3,1))  # the start position in re^3
-        # self.chi_s = np.inf  # the start course angle
-        self.p_e = np.inf*np.ones((3,1))  # the end position in re^3
-        # self.chi_e = np.inf  # the end course angle
-        # self.radius = np.inf  # turn radius
-        # self.length = np.inf  # length of the Dubins path
-        self.center_s = np.inf*np.ones((3,1))  # center of the start circle
-        self.dir_s = np.inf  # direction of the start circle
-        self.center_e = np.inf*np.ones((3,1))  # center of the end circle
-        self.dir_e = np.inf  # direction of the end circle
-        self.r1 = np.inf*np.ones((3,1))  # vector in re^3 defining half plane H1
-        self.r2 = np.inf*np.ones((3,1))  # vector in re^3 defining position of half plane H2
-        self.r3 = np.inf*np.ones((3,1))  # vector in re^3 defining position of half plane H3
-        self.n1 = np.inf*np.ones((3,1))  # unit vector in re^3 along straight line path
-        self.n3 = np.inf*np.ones((3,1))  # unit vector defining direction of half plane H3
-
-    # def update(self, ps, chis, pe, chie, R):
-    def update(self, ps, pe, R):
+    @staticmethod
+    def update(ps, pe, R):
         """
         Update the dubins parameters.
 
@@ -432,17 +420,8 @@ class DubinsParameters:
             _module_logger.warning('Error in Dubins Parameters')
             return DubinsResult(valid=False)
         else:
-            # self.p_s = ps
             chis = ps.chi
-            # self.p_e = pe
             chie = pe.chi
-            # self.radius = R
-
-            # self.p_s = ps
-            # self.chi_s = chis
-            # self.p_e = pe
-            # self.chi_e = chie
-            # self.radius = R
 
             ps = np.array([[ps.n, ps.e, ps.d]]).T
             pe = np.array([[pe.n, pe.e, pe.d]]).T
@@ -451,13 +430,6 @@ class DubinsParameters:
             c_ls = ps + R * rotz(-np.pi/2.).dot(np.array([[np.cos(chis), np.sin(chis), 0.]]).T)
             c_re = pe + R * rotz(np.pi/2.).dot(np.array([[np.cos(chie), np.sin(chie), 0.]]).T)
             c_le = pe + R * rotz(-np.pi/2.).dot(np.array([[np.cos(chie), np.sin(chie), 0.]]).T)
-
-            # print(ps, c_rs, chis)
-            # print(ps, c_ls, chis)
-            # print(pe, c_re, chie)
-            # print(pe, c_le, chie)
-            # import sys
-            # sys.exit()
 
             theta = np.arctan2(c_re.item(1) - c_rs.item(1), c_re.item(0) - c_rs.item(0))
             L1 = np.linalg.norm(c_rs - c_re) + R*mod(2.*np.pi + mod(theta - np.pi/2.) - mod(chis - np.pi/2.)) + R * mod(2.*np.pi + mod(chie - np.pi/2.) - mod(theta - np.pi/2.))
@@ -479,64 +451,57 @@ class DubinsParameters:
 
             lengths = [L1, L2, L3, L4] 
             L = min(lengths)
-            # self.length = lengths[int(L)]
-            # self.length = L
 
             e1 = np.array([[1, 0, 0]]).T
 
             if L == L1:
-                self.center_s = c_rs
-                self.dir_s = 1
-                self.center_e = c_re
-                self.dir_e = 1
-                self.n1 = (self.center_e - self.center_s) / np.linalg.norm(self.center_e - self.center_s)
-                self.r1 = self.center_s + R*rotz(-np.pi/2.).dot(self.n1)
-                self.r2 = self.center_e + R*rotz(-np.pi/2.).dot(self.n1)
+                center_s = c_rs
+                dir_s = 1
+                center_e = c_re
+                dir_e = 1
+                n1 = (center_e - center_s) / np.linalg.norm(center_e - center_s)
+                r1 = center_s + R*rotz(-np.pi/2.).dot(n1)
+                r2 = center_e + R*rotz(-np.pi/2.).dot(n1)
             
             if L == L2:
-                self.center_s = c_rs
-                self.dir_s = 1
-                self.center_e = c_le
-                self.dir_e = -1
-                ell = np.linalg.norm(self.center_e - self.center_s)
-                theta = np.arctan2(self.center_e.item(1) - self.center_s.item(1), self.center_e.item(0) - self.center_s.item(0))
+                center_s = c_rs
+                dir_s = 1
+                center_e = c_le
+                dir_e = -1
+                ell = np.linalg.norm(center_e - center_s)
+                theta = np.arctan2(center_e.item(1) - center_s.item(1), center_e.item(0) - center_s.item(0))
                 arg = np.clip(2.*R/ell, -1., 1.)
                 theta2 = theta - np.pi/2. + np.arcsin(arg)
-                self.n1 = rotz(theta2 + np.pi/2.).dot(e1)
-                self.r1 = self.center_s + R*rotz(theta2).dot(e1)
-                self.r2 = self.center_e + R*rotz(theta2 + np.pi).dot(e1)
+                n1 = rotz(theta2 + np.pi/2.).dot(e1)
+                r1 = center_s + R*rotz(theta2).dot(e1)
+                r2 = center_e + R*rotz(theta2 + np.pi).dot(e1)
 
             if L == L3:
-                self.center_s = c_ls
-                self.dir_s = -1
-                self.center_e = c_re
-                self.dir_e = 1
-                ell = np.linalg.norm(self.center_e - self.center_s)
-                theta = np.arctan2(self.center_e.item(1) - self.center_s.item(1), self.center_e.item(0) - self.center_s.item(0))
+                center_s = c_ls
+                dir_s = -1
+                center_e = c_re
+                dir_e = 1
+                ell = np.linalg.norm(center_e - center_s)
+                theta = np.arctan2(center_e.item(1) - center_s.item(1), center_e.item(0) - center_s.item(0))
                 arg = np.clip(2.*R/ell, -1., 1.)
                 theta2 = np.arccos(arg)
-                self.n1 = rotz(theta + theta2 - np.pi/2.).dot(e1)
-                self.r1 = self.center_s + R*rotz(theta + theta2).dot(e1)
-                self.r2 = self.center_e + R*rotz(theta + theta2 - np.pi).dot(e1)
+                n1 = rotz(theta + theta2 - np.pi/2.).dot(e1)
+                r1 = center_s + R*rotz(theta + theta2).dot(e1)
+                r2 = center_e + R*rotz(theta + theta2 - np.pi).dot(e1)
 
             if L == L4:
-                self.center_s = c_ls
-                self.dir_s = -1
-                self.center_e = c_le
-                self.dir_e = -1
-                self.n1 = (self.center_e - self.center_s) / np.linalg.norm(self.center_e - self.center_s)
-                self.r1 = self.center_s + R*rotz(np.pi/2.).dot(self.n1)
-                self.r2 = self.center_e + R*rotz(np.pi/2.).dot(self.n1)
+                center_s = c_ls
+                dir_s = -1
+                center_e = c_le
+                dir_e = -1
+                n1 = (center_e - center_s) / np.linalg.norm(center_e - center_s)
+                r1 = center_s + R*rotz(np.pi/2.).dot(n1)
+                r2 = center_e + R*rotz(np.pi/2.).dot(n1)
 
-            self.r3 = pe
-            self.n3 = rotz(chie).dot(e1)
+            r3 = pe
+            n3 = rotz(chie).dot(e1)
             
-            return DubinsResult(ps, chis, self.center_s, self.r1, self.dir_s, pe, chie, self.center_e, self.r2, self.dir_e, True)
-        # self.center_s = Waypoint(self.center_s.item(0), self.center_s.item(1), self.center_s.item(2))
-        # self.center_e = Waypoint(self.center_e.item(0), self.center_e.item(1), self.center_e.item(2))
-        # self.r1 = Waypoint(self.r1.item(0), self.r1.item(1), self.r1.item(2))
-        # self.r2 = Waypoint(self.r2.item(0), self.r2.item(1), self.r2.item(2))
-        # self.r3 = Waypoint(self.r3.item(0), self.r3.item(1), self.r3.item(2))
+            return DubinsResult(ps, pe, chis, chie, center_s, center_e, dir_s, dir_e, r1, r2, r3, n1, n3, True)
 
 
 class DubinsResult(object):
@@ -547,26 +512,49 @@ class DubinsResult(object):
     ----------
     ps : metis.location.Waypoint
         A waypoint for the starting position in the path.
+    pe : metis.location.Waypoint
+        A waypoint for the ending position in the path.
     cs : metis.location.NEDPoint
         A NED point representing the center of the first arc traced out in the path.
+    ce : metis.location.NEDPoint
+        A NED point representing the center of the second arc traced out in the path.
+    ds : int, (-1 or 1)
+        The sense of rotation around the first arc, (1 = clockwise, -1 = counterclockwise).
+    de : int, (-1 or 1)
+        The sense of rotation around the second arc, (1 = clockwise, -1 = counterclockwise).
     r1 : metis.location.Waypoint
         A waypoint for the position in the path transitioning from the first
         arc to the straight line segment.
-    ds : int, (-1 or 1)
-        The sense of rotation around the first arc, (1 = clockwise, -1 = counterclockwise).
-    pe : metis.location.Waypoint
-        A waypoint for the ending position in the path.
-    ce : metis.location.NEDPoint
-        A NED point representing the center of the second arc traced out in the path.
     r2 : metis.location.Waypoint
         A waypoint for the position in the path transitioning from the  
         straight line segment to the second arc.
-    de : int, (-1 or 1)
-        The sense of rotation around the second arc, (1 = clockwise, -1 = counterclockwise).
+    r3 : metis.location.Waypoint
+        A waypoint for the position in the path transitioning from the  
+        second arc out of the dubins path.
+    n1 : np.ndarray
+        A (3x1) array representing a vector normal to the halfplane leaving 
+        the first arc.
+    n3 : np.ndarray
+        A (3x1) array representing a vector normal to the halfplane leaving 
+        the Dubins path.
     valid : bool
         A flag showing whether the dubins parameters were calculated successfully.
     """
-    def __init__(self, ps=None, chis=None, cs=None, r1=None, ds=None, pe=None, chie=None, ce=None, r2=None, de=None, valid=False):
+    def __init__(self, 
+        ps=None, 
+        pe=None, 
+        chis=None, 
+        chie=None,
+        cs=None, 
+        ce=None, 
+        ds=None,
+        de=None, 
+        r1=None, 
+        r2=None, 
+        r3=None,
+        n1=None,
+        n3=None,
+        valid=False):
         """
         A data object holding the values that define a Dubins path.
 
@@ -604,13 +592,18 @@ class DubinsResult(object):
         if valid:
             self.ps = Waypoint(ps.item(0), ps.item(1), ps.item(2), chis)
             self.cs = NEDPoint(cs.item(0), cs.item(1), cs.item(2))
-            self.r1 = Waypoint(r1.item(0), r1.item(1), r1.item(2))
             self.ds = ds
 
             self.pe = Waypoint(pe.item(0), pe.item(1), pe.item(2), chie)
             self.ce = NEDPoint(ce.item(0), ce.item(1), ce.item(2))
-            self.r2 = Waypoint(r2.item(0), r2.item(1), r2.item(2))
             self.de = de
+
+            self.r1 = Waypoint(r1.item(0), r1.item(1), r1.item(2))
+            self.r2 = Waypoint(r2.item(0), r2.item(1), r2.item(2))
+            self.r3 = Waypoint(r3.item(0), r3.item(1), r3.item(2))
+
+            self.n1 = n1
+            self.n3 = n3
 
 
 def rotz(theta):
