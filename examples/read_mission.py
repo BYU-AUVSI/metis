6964771da2fa10bedd032549d7f9fa20fe924171
Sequoia Ploeg <sequoiap4@gmail.com>
2020-04-21 10:31:03 -0600

    Plots mission

--- examples/read_mission.py
+++ examples/read_mission.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from metis.manager import MissionManager
+from metis.rrt.animation import Animation2D
 
 with open('./mission.pkl', 'r') as f:
     mission = pickle.load(f)
 
-print(mission)
+# print(mission)
 
-manager = MissionManager(mission)
+# manager = MissionManager(mission)
 
-plan = manager.plan_objective()
-plan.plot()
+# plan = manager.plan_objective()
+# plan.plot()
+
+ani = Animation2D(mission)
